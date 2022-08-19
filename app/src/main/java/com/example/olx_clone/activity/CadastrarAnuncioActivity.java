d35b619dfb2039a0fd452d544ff30f1294634122
imiranda <igor.miranda@foursys.com.br>
2022-08-19 15:38:00 -0300

    mascara de dinheiro adicionada

--- app/src/main/java/com/example/olx_clone/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/olx_clone/activity/CadastrarAnuncioActivity.java
@@ -1,8 +1,12 @@
 package com.example.olx_clone.activity;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.olx_clone.databinding.ActivityCadastrarAnuncioBinding;
 import com.example.olx_clone.databinding.ActivityCadastroBinding;
@@ -17,5 +21,19 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityCadastrarAnuncioBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.btnCadastarAnucio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                salvarAnuncio(view);
+            }
+        });
+
+    }
+
+    public void salvarAnuncio(View view){
+
+        String valor = binding.edtValor.getHintString();
+        Log.d("salvar", "salvarAnuncio: "+valor);
     }
 }
